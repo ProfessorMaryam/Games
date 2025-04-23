03ecd9b409686a4e21df4b46264c3466498899bc
ProfessorMaryam <>
2025-04-23 19:35:50 +0300

    MY GOOOATTTT

---
+++ Assets/MusicNotesScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicNotesScript : MonoBehaviour
+{
+    public AudioSource C_Note;
+
+
+
+    public void C_Note_Play()
+    {
+        C_Note.Play();
+    }
+}
